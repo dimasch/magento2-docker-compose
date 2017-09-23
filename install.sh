a46727466b4d166bb1796abf52bd53faad8d573d
Dmitry Schegolikhin <d.schegolikhin@kt-team.de>
2017-09-23 22:53:27 +0400

    Map host data to volumes.

--- install.sh
+++ install.sh
@@ -10,12 +10,15 @@ cp -f docker-compose.override.yml.setup docker-compose.override.yml
 echo "Start setup process.."
 docker-compose run --rm setup
 
-echo "Setup complete.."
-
+echo "Up app.."
 docker-compose up -d app
 
 echo "Copy site data to host.."
 cid=$(docker-compose ps -q phpfpm)
 docker cp $cid:/var/www/html ./
 
+echo "Map host data to volumes.."
+cp -f docker-compose.override.yml.dist docker-compose.override.yml
+docker-compose up -d app
 
+echo "Setup complete.."
